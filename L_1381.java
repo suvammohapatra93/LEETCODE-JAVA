241e57d2126b474a137c4fc0baf6d842a7d731e6
suvammohapatra93 <suvammohapatra93480@gmail.com>
2024-09-30 17:29:44 +0530

    Date->30-09-24

---
+++ L_1381.java
@@ -0,0 +1,35 @@
+
+// LEETCODE -> Design A stack with increament Operation
+
+class L_1381 {
+
+    // Array to store stack elements
+    private int[] stackArray;
+    // Index of the top element in the stack
+    private int topIndex;
+
+    public L_1381(int maxSize) {
+        stackArray = new int[maxSize];
+        topIndex = -1;
+    }
+
+    public void push(int x) {
+        if (topIndex < stackArray.length - 1) {
+            stackArray[++topIndex] = x;
+        }
+    }
+
+    public int pop() {
+        if (topIndex >= 0) {
+            return stackArray[topIndex--];
+        }
+        return -1; // Return -1 if the stack is empty
+    }
+
+    public void increment(int k, int val) {
+        int limit = Math.min(k, topIndex + 1);
+        for (int i = 0; i < limit; i++) {
+            stackArray[i] += val;
+        }
+    }
+}
